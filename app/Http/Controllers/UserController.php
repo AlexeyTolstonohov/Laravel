be6eba0c497e02986e7e39c7dc2d72819dd20db9
AlexeyTolstonohov <tolstonohov.alex@gmail.com>
2021-05-11 17:04:36 +0300

    reg

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class UserController extends Controller
+
+
 {
     public function createUser(){
 
-        return view('user.create');
+        return view('user.createUser');
     }
 
     public function storeUser(Request $request){
-        dd($request->all());
+
+        $request->validate([
+            'name' => 'requared',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+            session()->flash('success', 'succsessful registration');
+        Auth::login($user);
+        return redirect()->home();
     }
 }
---
+++ app/Providers/UserProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class User2 extends Controller{
+
+    public $data =[
+        'name', 'email', 'password'
+    ];
+
+    public function create2($name, $email, $password){
+        $this->name = $name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function create($data){
+        $this->name = $data->name;
+        $this->email = $data->email;
+        $this->password = $data->password;
+    }
+}
+?>
