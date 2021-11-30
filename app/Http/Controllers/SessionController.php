c88e45da679578b3cbe6632bfca9ee647fcac5ce
AlexeyTolstonohov <tolstonohov.alex@gmail.com>
2021-12-01 01:22:55 +0200

    formating PSR-2 standart

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -7,44 +7,40 @@ use App\Http\Controllers\Controller;
 
 class SessionController extends Controller {
 
-   public function accessSessionData(Request $request) {
-      if($request->session()->has('my_name'))
-         echo $request->session()->get('my_name');
-      else
-         echo 'No data in the session';
-         dump(session()->all());
-   }
-
-   public function storeSessionData(Request $request) {
-      $request->session()->put('my_name','Virat Gandhi');
-      echo "Data has been added to session";
-      dump(session()->all());
-   }
-
-   public function deleteSessionData(Request $request) {
-      $request->session()->forget('my_name');
-      echo "Data has been removed from session.";
-   }
-
-
-
-    public function show(Request $request){
+    public function accessSessionData(Request $request) {
+        if($request->session()->has('my_name'))
+        {
+            echo $request->session()->get('my_name');
+        } else {
+            echo 'No data in the session';
+        }
 
         dump(session()->all());
     }
 
-    public function put(Request $request){
+    public function storeSessionData(Request $request) {
+        $request->session()->put('my_name','Virat Gandhi');
+        echo "Data has been added to session";
+        dump(session()->all());
+    }
 
+    public function deleteSessionData(Request $request) {
+        $request->session()->forget('my_name');
+        echo "Data has been removed from session.";
+    }
+
+    public function show(Request $request){
+        dump(session()->all());
+    }
+
+    public function put(Request $request){
         $request->session()->put('test', 'Test value');
         dump(session()->all());
     }
 
     public function get(Request $request){
-
         $request->session()->get('test');
-       // dump(session()->all());
-       dump($request->session()->get('test'));
+        dump($request->session()->get('test'));
     }
-
 }
 
